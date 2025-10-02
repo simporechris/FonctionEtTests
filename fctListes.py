90c7776581c376bdc42802a2846630245baadc4e
vivgxojo <maryse.mongeau@cegepoutaouais.qc.ca>
2025-10-02 14:47:02 -0400

    Initial commit

---
+++ fctListes.py
@@ -0,0 +1,24 @@
+# Pour chaque fonction une personne différente va :
+# a. Programmer la fonction
+# b. Créer le plan de tests
+# c. Programmer les tests
+
+"""
+1. Fonction qui reçoit les points de vies et les points de défenses d'un joueur et les points d'attaques de l'autre et
+qui retourne les points de vies restants après l'attaque.
+"""
+
+"""
+2. Fonction qui reçoit une liste de legos et une couleur et qui le pourcentage de blocs de cette couleur
+"""
+
+"""
+3. Fonction qui reçoit une liste de legos et qui il y a combien de blocs de chaque couleur en moyenne
+"""
+
+# Répartition des tâches :
+"""                     a   b   c
+Nom :                   1   2   3
+Nom :                   2   3   1
+Nom :                   3   1   2
+"""
